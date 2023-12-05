5681608943a87e96bed070fa9c289ce27f603ad4
Konstantinos <kostantinos.vanakas@ulb.be>
2023-12-05 14:39:29 +0100

    correct grid and file of the cards

--- tetramino.py
+++ tetramino.py
@@ -1,33 +1,76 @@
 
-class Tetramino:
-    def __init__(self):
+"""class Tetramino:"""
+"""    def __init__(self):
         self.tetraminos = []
         self.grid = []
-        self.file_path = ""
-    def create_grid(self, w: int, h: int):
-        """Crée une grille de taille (3w+2)×(3h+2) , comprenant les délimitations de la zone centrale."""
-        grid = [['  ' for _ in range(3 * w + 2)] for _ in range(3 * h + 2)]
-        for i in range(w + 1, 2 * w + 1):
-            grid[h][i] = "--"
-        for j in range(h + 1, 2 * h + 1):
-            grid[j][w] = "|"
-            grid[j][2 * w + 1] = "|"
-        return grid
+        self.file_path = """""
 
-    def print_grid(self, grid: list(list), no_number: bool):
-        """Print le plateau de jeu. Pour améliorer la jouabilité, les limites du plateau doivent être
-    affichées en utilisant les caractères ’|’ (bordures verticales) et (-) (bordures horizontales).
-    Référez-vous aux figures des sections précédentes pour un exemple (par exemple : Figure 1).
-    Le paramètre no_number indique si les pièces doivent être affichées avec (si sa valeur est
-    False) ou sans (si sa valeur est True) les chiffres correspondants.
-    """
-        for i in range(len(grid)):
-            for j in range(len(grid[i])):
-                print(grid[i][j], end='')
-            print()
+def import_card(file_path: str):
+    """Permet l’import d’un fichier de jeu. Cette fonction ouvre le fichier, en extrait le contenu et
+le traite de manière a générer les tétraminos définis ainsi que les dimensions du plateau."""
 
 
-'''    def import_card(self, file_path: str):
+
+def create_grid(w, h):
+    """Crée une grille de taille (3w+2)×(3h+2) , comprenant les délimitations de la zone centrale."""
+    global board
+    full_w = (3 * w) + 2
+    full_h = (3 * h) + 2
+    board = []
+    for i in range(full_h):
+        line = []
+        if i < h:
+            for j in range(full_w):
+                line.append('  ')
+            board.append(line)
+        elif i == h:
+            for l in range(w):
+                line.append('  ')
+            for k in range(w + 1, 2 * w + 1):
+                line.append('--')
+            for m in range(2 * w + 1, full_w+1):
+                line.append('  ')
+            board.append(line)
+        elif h < i < 2 * h + 1:
+            for j in range(full_w):
+                if j == w-1:
+                    line.append(' |')
+                elif j == 2 * w:
+                    line.append('| ')
+                else:
+                    line.append('  ')
+            board.append(line)
+        elif i == 2 * h + 1:
+            for l in range(w):
+                line.append('  ')
+            for k in range(w + 1, 2 * w + 1):
+                line.append('--')
+            for m in range(2 * w + 1, full_w + 1):
+                line.append('  ')
+            board.append(line)
+        elif 2 * h + 1 < i < full_h:
+            for j in range(full_w):
+                line.append('  ')
+            board.append(line)
+    return board
+
+grid = create_grid(5, 4)
+for line in grid:
+    print(line)
+
+
+
+
+
+
+
+
+
+
+
+
+
+'''
 
     def setup_tetraminos(self, tetraminos: list(tetramino), grid: list(list)):
 
@@ -38,10 +81,6 @@ class Tetramino:
     def check_move(self, tetramino: tetramino, grid: list(list)):
 
     def  check_win(self, grid: list(list)):'''
-
-'''def main():
-    game = Tetramino()'''
-
 
 
 
