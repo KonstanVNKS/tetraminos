105f9af018440546131288c19ef6c294b009e3f1
Konstantinos <114684466+KonstanVNKS@users.noreply.github.com>
2023-12-16 20:02:30 +0100

    Update tetramino.py

--- tetramino.py
+++ tetramino.py
@@ -1,12 +1,6 @@
 import sys
 from getkey import getkey
-"""class Tetramino:"""
-"""    def __init__(self):
-        self.tetraminos = []
-        self.grid = []
-        self.file_path = """""
 
-file_path = 'carte_1.txt'
 def import_card(file_path: str):
     """Permet l’import d’un fichier de jeu. Cette fonction ouvre le fichier, en extrait le contenu et
     le traite de manière a générer les tétraminos définis ainsi que les dimensions du plateau.Return:
@@ -17,14 +11,22 @@ def import_card(file_path: str):
     dimensions = tuple(map(int, lignes[0].strip().split(',')))
     tetraminos = []
     for line in lignes[1:]:
+        tetramino = []
         data = line.strip().split(';;')
-        pos_t = data[0].split(';')
+        pos_t = []
+        for coord in data[0].split(';'):
+            coord_vals = tuple(int(val) for val in coord.strip('()').split(','))
+            pos_t.append(coord_vals)
         color = data[1]
-        offset = (0, 0) # default value
-        tetraminos.append((pos_t, color, offset))
+        offset = (0, 0)
+        tetramino.append(pos_t)
+        tetramino.append(color)
+        tetramino.append(offset)
+        tetraminos.append(tetramino)
     return dimensions, tetraminos
 
-def create_grid(w, h):
+
+def create_grid(w :int, h: int):
     """Crée une grille de taille (3w+2)×(3h+2) , comprenant les délimitations de la zone centrale."""
     full_w = (3 * w) + 2
     full_h = (3 * h) + 2
@@ -69,49 +71,57 @@ def create_grid(w, h):
 
 grid = create_grid(5, 4)
 
+
 def print_grid(grid):
     """Affiche la grille de jeu dans la console."""
     for line in grid:
         print(line)
 
 
-def setup_tetraminos(tetraminos, grid):
-    """Mets en place les tétraminos importés depuis le fichier chacun dans leur sous-matrice sur la
-matrice principale. Cette fonction est appelée une seule fois en début de jeu pour initialiser la grille de départ.
-Return:(list(list), list(tetramino)) → Nouvelle grille avec les tetraminos placés, et la liste de tétraminos avec
-leur nouvelle position mise à jour"""
-    new_grid = [row[:] for row in grid]  # Copie de la grille pour éviter la modification de la grille originale
-    updated_tetraminos = []
-    for tetramino in tetraminos:
-        coordinates, color = tetramino[:2]  # Récupérer les coordonnées et la couleur
+def setup_color(tetraminos: list[tuple, str, tuple]):
+    for index, tetramino in enumerate(tetraminos):
+        color = tetramino[1]
+        color_code = '\x1b[' + str(color) + 'm' + str(index) + '\x1b[0m'
 
-        if len(tetramino) > 2:  # Vérifier s'il y a un décalage
-            offset = tetramino[2]
-            x_offset, y_offset = offset
-        else:
-            x_offset, y_offset = 0, 0
-
-        # Placer le tétraminos dans sa sous-matrice respective sur la grille
-        for coord in coordinates:
-            x, y = coord
-            new_grid[x + x_offset][y + y_offset] = color
-
-        # Mettre à jour les coordonnées du tétraminos avec son nouvel emplacement
-        updated_coordinates = [(x + x_offset, y + y_offset) for x, y in coordinates]
-        updated_tetraminos.append((updated_coordinates, color, offset))
-
-    return new_grid, updated_tetraminos
+    return color_code
 
 
-print(setup_tetraminos(import_card(file_path), grid))
+def setup_tetraminos(tetraminos: list[tuple[int,int], str, tuple[int,int]], grid: list[list]):
+    w, h = import_card(file_path)
+    new_grid = [row[:] for row in grid]
+    for index, tetramino in enumerate(tetraminos):
+        color = setup_color(tetraminos)
+        for coord in tetramino[0]:
+            if index == 0:
+                grid[coord[0]][coord[1]] = color
+                tetramino[2] = (0, 0)
+            elif index == 1:
+                grid[coord[0]][coord[1]] = color
+                tetramino[2] = (w+1, 0)
+            elif index == 2:
+                grid[coord[0]][coord[1]] = color
+                tetramino[2] = (2*w + 2, 0)
+            elif index == 3:
+                grid[coord[0]][coord[1]] = color
+                tetramino[2] = (0, h+1)
+            elif index == 4:
+                grid[coord[0]][coord[1]] = color
+                tetramino[2] = (2*w+2, h+1)
+            elif index == 5:
+                grid[coord[0]][coord[1]] = color
+                tetramino[2] = (0, 2*h+2)
+            elif index == 6:
+                grid[coord[0]][coord[1]] = color
+                tetramino[2] = (w+1, 2*h+2)
+            elif index == 7:
+                grid[coord[0]][coord[1]] = color
+                tetramino[2] = (2*w+2, 2*h+2)
+    return grid, tetraminos
 
 
 
 
 '''
-
-    def setup_tetraminos(tetraminos: list(tetramino), grid: list(list)):
-
     def place_tetraminos(tetraminos: list(tetramino), grid: list(list)):
 
     def rotate_tetramino(tetramino: tetramino, clockwise: bool (default=True)):
